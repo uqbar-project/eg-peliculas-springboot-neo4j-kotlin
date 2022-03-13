db6e9cd26a21e25df5103743116f47a48f6459b0
Fernando Dodino <fernando.dodino@gmail.com>
2022-03-13 11:55:14 -0300

    Sorting actors by id in query to avoid flaky tests

--- src/main/kotlin/ar/edu/algo3/peliculas/repository/PeliculasRepository.kt
+++ src/main/kotlin/ar/edu/algo3/peliculas/repository/PeliculasRepository.kt
@@ -12,7 +12,7 @@ interface PeliculasRepository : Neo4jRepository<Pelicula, Long>  {
     @Query("MATCH (pelicula:Movie) WHERE pelicula.title =~ \$titulo RETURN pelicula LIMIT 10")
     fun peliculasPorTitulo(titulo: String): List<Pelicula>
 
-    @Query("MATCH (pelicula:Movie)<-[actuo_en:ACTED_IN]-(persona:Person) WHERE ID(pelicula) = \$id RETURN pelicula, collect(actuo_en), collect(persona) LIMIT 1")
+    @Query("MATCH (pelicula:Movie)<-[actuo_en:ACTED_IN]-(persona:Person) WITH pelicula, actuo_en, persona ORDER BY ID(persona) WHERE ID(pelicula) = \$id RETURN pelicula, COLLECT(actuo_en), COLLECT(persona) LIMIT 1")
     fun pelicula(id: Long): Optional<Pelicula>
 
 }
