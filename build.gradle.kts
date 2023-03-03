10e44f831f0f72b06c2f01d3140477b38f45fe39
Fernando Dodino <fernando.dodino@gmail.com>
2023-03-03 19:58:57 -0300

    Using Spring Boot 3.0.2, Kotlin 1.8.10 & docker-compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,59 +1,73 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.4"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
-    kotlin("plugin.jpa") version "1.6.10"
+    id("org.springframework.boot") version "3.0.2"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.spring") version "1.8.10"
+    kotlin("plugin.jpa") version "1.8.10"
     jacoco
 }
 
+extra["springCloudVersion"] = "2022.0.0"
+
 group = "org.uqbar"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_14
-java.targetCompatibility = JavaVersion.VERSION_14
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
-val springVersion = "2.6.7"
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter:$springVersion")
-    implementation("org.springframework.boot:spring-boot-starter-web:$springVersion")
-    implementation("org.springframework.boot:spring-boot-starter-hateoas:$springVersion")
-    implementation("org.springframework.boot:spring-boot-starter-data-neo4j:$springVersion")
+    // básicos de cualquier proyecto Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
-    implementation("org.springframework.boot:spring-boot-devtools:$springVersion")
-    testImplementation("org.springframework.boot:spring-boot-starter-test:$springVersion")
-    testImplementation("org.neo4j:neo4j-ogm-embedded-driver:3.2.33")
-    testImplementation("org.neo4j.test:neo4j-harness:4.4.6")
-    testImplementation("org.neo4j.driver:neo4j-java-driver-test-harness-spring-boot-autoconfigure:4.3.6.0")
+    implementation("org.springframework.boot:spring-boot-starter-web-services")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.14.2")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
+    implementation("org.springframework.boot:spring-boot-devtools")
+
+    // conexión a la base
+    implementation("org.springframework.boot:spring-boot-starter-data-neo4j")
+
+    // testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.neo4j:neo4j-ogm-embedded-driver:3.2.39")
+    testImplementation("org.neo4j.test:neo4j-harness:5.5.0")
+//    testImplementation("org.neo4j.driver:neo4j-java-driver-test-harness-spring-boot-autoconfigure:5.5.0")
+    testImplementation("org.springframework.boot:spring-boot-autoconfigure:3.0.3")
 }
 
 configurations {
     all {
-        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
-        exclude(group = "ch.qos.logback", module = "logback-classic")
+        exclude("org.springframework.boot","spring-boot-starter-logging")
+        exclude("ch.qos.logback", "logback-classic")
+    }
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
     }
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "14"
+        jvmTarget = "17"
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-    testLogging {
-        showStandardStreams = true
-    }
 }
 
 tasks.test {
@@ -65,7 +79,7 @@ tasks.jacocoTestReport {
 }
 
 jacoco {
-    toolVersion = "0.8.7"
+    toolVersion = "0.8.8"
 }
 
 tasks.jacocoTestReport {
@@ -88,4 +102,3 @@ tasks.register("runOnGitHub") {
     group = "custom"
     description = "$ ./gradlew runOnGitHub # runs on GitHub Action"
 }
-
